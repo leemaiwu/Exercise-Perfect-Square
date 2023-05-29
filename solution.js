a922c3d8502f26c6526e53059f366d6d230d8da0
Laura Wu <leemaiwu@gmail.com>
2023-05-28 22:24:37 -0600

    Set up

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
 function findNextSquare(sq) {
-  // Return the next square if sq is a perfect square, -1 otherwise
+  
   return -1;
 }
 
+console.log(findNextSquare(4))
+console.log(findNextSquare(144))
