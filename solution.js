c704f5e2b4f1adf4b78e71e08b408c9db92a613a
Laura Wu <leemaiwu@gmail.com>
2023-05-29 22:35:30 -0600

    Solved with sqrt and ternary

--- solution.js
+++ solution.js
@@ -1,7 +1,7 @@
 function findNextSquare(sq) {
-  
-  return -1;
+  let root = Math.sqrt(sq)
+  return root % 1 === 0 ? (root + 1)*(root + 1) : -1
 }
 
-console.log(findNextSquare(4))
-console.log(findNextSquare(144))
+console.log(findNextSquare(121)) // 144
+console.log(findNextSquare(155)) // -1
